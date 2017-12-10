22c5bd1a6cae2b31d4ebbc5fb7d226da2bdcd246
Halil Gursoy <halilgursoy91@gmail.com>
2017-12-10 11:44:38 +0100

    Fix UI bugs in the extension

--- Open in der Satz/ActionViewController.swift
+++ Open in der Satz/ActionViewController.swift
@@ -52,11 +52,4 @@ class ActionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
-    }
-
 }
--- Texture/UI/Utility/UIKitExtensions.swift
+++ Texture/UI/Utility/UIKitExtensions.swift
@@ -34,6 +34,12 @@ extension UIViewController {
         }
         completion(nil)
     }
+    
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
 }
 
 // MARK: - UI extensions of UIViewController
