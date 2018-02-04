b9b46082dc6932bc519e7030e1f7aae2445ee37c
Halil Gursoy <halilgursoy91@gmail.com>
2018-02-04 21:43:31 +0100

    Add version and build to settings footer

--- Texture/UI/Utility/FoundationExtensions.swift
+++ Texture/UI/Utility/FoundationExtensions.swift
@@ -39,3 +39,20 @@ extension String {
         return ceil(boundingBox.width)
     }
 }
+
+extension UIApplication {
+    
+    class func appVersion() -> String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    }
+    
+    class func appBuild() -> String {
+        return Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    }
+    
+    class func versionBuild() -> String {
+        let version = appVersion(), build = appBuild()
+        
+        return version == build ? "v\(version)" : "v\(version)(\(build))"
+    }
+}
